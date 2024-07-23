ee10950bae7ada5261dd743cf1ea7cce8d7ea52c
fatima.hakimi <fatimahakimi96@gmail.com>
2024-07-22 19:20:39 -0600

    Address and AddressDTO has been started.

---
+++ src/main/java/com/cydeo/dto/AddressDTO.java
@@ -0,0 +1,86 @@
+package com.cydeo.dto;
+
+
+
+    public class AddressDTO {
+
+        private Long id;
+        private String addressLine1;
+        private String addressLine2;
+        private String city;
+        private String state;
+        private String country;
+        private String zipCode;
+
+        // Constructors
+        public AddressDTO() {}
+
+        public AddressDTO(Long id, String addressLine1, String addressLine2, String city, String state, String country, String zipCode) {
+            this.id = id;
+            this.addressLine1 = addressLine1;
+            this.addressLine2 = addressLine2;
+            this.city = city;
+            this.state = state;
+            this.country = country;
+            this.zipCode = zipCode;
+        }
+
+        // Getters and Setters
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getAddressLine1() {
+            return addressLine1;
+        }
+
+        public void setAddressLine1(String addressLine1) {
+            this.addressLine1 = addressLine1;
+        }
+
+        public String getAddressLine2() {
+            return addressLine2;
+        }
+
+        public void setAddressLine2(String addressLine2) {
+            this.addressLine2 = addressLine2;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getZipCode() {
+            return zipCode;
+        }
+
+        public void setZipCode(String zipCode) {
+            this.zipCode = zipCode;
+        }
+    }
+
+
