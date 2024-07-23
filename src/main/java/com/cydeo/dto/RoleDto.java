a8e1c26dffe2548d8efbf5fcc988f371be17283e
mahdia <mahdia.nemati1@gmail.com>
2024-07-22 19:40:06 -0600

    Role and RoleDTO has been updated.

--- src/main/java/com/cydeo/dto/RoleDto.java
+++ src/main/java/com/cydeo/dto/RoleDto.java
@@ -1,4 +1,23 @@
 package com.cydeo.dto;
 
+import com.cydeo.entity.common.BaseEntity;
+
+import javax.persistence.Entity;
+
+import lombok.*;
+
+import javax.persistence.Table;
+
 public class RoleDto {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Entity
+    @Table(name = "roles")
+    public class Role extends BaseEntity {
+
+        private long id;
+        private String description;
+    }
 }
--- src/main/java/com/cydeo/entity/Role.java
+++ src/main/java/com/cydeo/entity/Role.java
@@ -4,6 +4,18 @@ import com.cydeo.entity.common.BaseEntity;
 
 import javax.persistence.Entity;
 
+import lombok.*;
+import javax.persistence.Table;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "roles")
 public class Role extends BaseEntity {
+
+    private String description;
 }
+
