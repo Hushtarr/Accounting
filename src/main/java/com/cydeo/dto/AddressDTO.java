013bba8e5a15c118507ed8ef62e7a3e79ad7d74d
fatima.hakimi <fatimahakimi96@gmail.com>
2024-07-23 07:58:13 -0600

    Address and AddressDTO has been completed.

--- src/main/java/com/cydeo/dto/AddressDTO.java
+++ src/main/java/com/cydeo/dto/AddressDTO.java
@@ -1,5 +1,14 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 
 public class AddressDTO {
 
@@ -11,76 +20,4 @@ public class AddressDTO {
     private String country;
     private String zipCode;
 
-    // Constructors
-    public AddressDTO() {
-    }
-
-    public AddressDTO(Long id, String addressLine1, String addressLine2, String city, String state, String country, String zipCode) {
-        setId(id);
-        setAddressLine1(addressLine1);
-        setAddressLine2(addressLine2);
-        setCity(city);
-        setState(state);
-        setCountry(country);
-        setZipCode(zipCode);
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddressLine1() {
-        return addressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
-
-    public String getAddressLine2() {
-        return addressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
-
-
--- src/main/java/com/cydeo/entity/Address.java
+++ src/main/java/com/cydeo/entity/Address.java
@@ -1,18 +1,24 @@
 package com.cydeo.entity;
 
+import com.cydeo.entity.common.BaseEntity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "address") //Entity table name should be "addresses"
-public class Address {
+@Getter
+@Setter
+@NoArgsConstructor
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+@Table(name = "addresses")
+
+public class Address extends BaseEntity {
+
+
 
     private String addressLine1;
     private String addressLine2;
@@ -21,74 +27,5 @@ public class Address {
     private String country;
     private String zipCode;
 
-    // Constructors
-    public Address() {
-    }
 
-    public Address(String addressLine1, String addressLine2, String city, String state, String country, String zipCode) {
-
-        setAddressLine1(addressLine1);
-        setAddressLine2(addressLine2);
-        setCity(city);
-        setState(state);
-        setCountry(country);
-        setZipCode(zipCode);
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAddressLine1() {
-        return addressLine1;
-    }
-
-    public void setAddressLine1(String addressLine1) {
-        this.addressLine1 = addressLine1;
-    }
-
-    public String getAddressLine2() {
-        return addressLine2;
-    }
-
-    public void setAddressLine2(String addressLine2) {
-        this.addressLine2 = addressLine2;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
