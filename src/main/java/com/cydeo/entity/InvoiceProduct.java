63228ea5a98db5b984321f20f924e66d0447434d
OneBitMance <danikalca443@gmail.com>
2024-07-23 04:43:26 +0200

    JDGR4 - InvoiceProduct and InvoiceProductDto classes created

--- src/main/java/com/cydeo/entity/InvoiceProduct.java
+++ src/main/java/com/cydeo/entity/InvoiceProduct.java
@@ -1,9 +1,36 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "invoice_products")
+@Where(clause = "is_deleted=false")
 public class InvoiceProduct extends BaseEntity {
+
+    private int quantity;
+    private BigDecimal price;
+    private int tax;
+    private BigDecimal profitLoss;
+    private int remainingQuantity;
+
+    @ManyToOne
+    private Invoice invoiceId;
+
+    @ManyToOne
+    private Product productId;
+
 }
