b1a8e847fb04d403fa06780116e0af19e5c79758
jaafar <noveenjaf@gmail.com>
2024-07-22 20:41:34 -0700

    invoice entity and DTO created

--- src/main/java/com/cydeo/dto/InvoiceDto.java
+++ src/main/java/com/cydeo/dto/InvoiceDto.java
@@ -1,4 +1,41 @@
 package com.cydeo.dto;
 
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvoiceDto {
+
+    private Long id;
+
+    private InvoiceStatus invoiceStatus;
+
+    private InvoiceType invoiceType;
+
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @NotNull
+    private CompanyDto company;
+
+    @NotNull
+    private ClientVendorDto clientVendor;
+
+    private BigDecimal price;
+
+    private BigDecimal tax;
+
+    private BigDecimal total;
+
+
 }
--- src/main/java/com/cydeo/entity/Invoice.java
+++ src/main/java/com/cydeo/entity/Invoice.java
@@ -1,9 +1,41 @@
 package com.cydeo.entity;
 
+import com.cydeo.dto.ClientVendorDto;
+import com.cydeo.dto.CompanyDto;
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.InvoiceStatus;
+import com.cydeo.enums.InvoiceType;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "invoices")
 public class Invoice extends BaseEntity {
+
+    private String invoiceNo;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceStatus invoiceStatus;
+
+    @Enumerated(EnumType.STRING)
+    private InvoiceType invoiceType;
+
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "client_vendor_id")
+    private ClientVendor clientVendor;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id")
+    private Company company;
+
 }
