060f0670758136fe5b49cd66858e66a74526f9bd
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-23 16:29:40 +0400

    Invoice converter, repository, service, controller classes created

---
+++ src/main/java/com/cydeo/service/impl/InvoiceServiceImp.java
@@ -0,0 +1,8 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.service.InvoiceService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InvoiceServiceImp implements InvoiceService {
+}
