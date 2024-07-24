663f7754a04eb8df76b9fce8a88377a9bf77677b
MBP <mamypala@hotmail.com>
2024-07-24 10:29:40 +0300

    JDGR4-19 - Category Repo,Service(Impl),Converter,Controller created-List All Categories done

---
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CategoryDto;
+import com.cydeo.entity.Category;
+import com.cydeo.repository.CategoryRepository;
+import com.cydeo.service.CategoryService;
+import com.cydeo.util.MapperUtil;
+
+import java.util.List;
+
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public List<CategoryDto> listAllCategories() {
+        return categoryRepository.findAll().stream()
+                .map(category -> mapperUtil.convert(category, new CategoryDto())).toList();
+    }
+
+    @Override
+    public CategoryDto findById(Long id) {
+        Category foundCategory = categoryRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+        return mapperUtil.convert(foundCategory, new CategoryDto());
+    }
+}
