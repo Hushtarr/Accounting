f4063d762d185877815d6530d6cec4f42016d1a8
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-24 08:11:42 +0400

    lombok added to InvoiceProductDto

--- src/main/java/com/cydeo/dto/InvoiceProductDto.java
+++ src/main/java/com/cydeo/dto/InvoiceProductDto.java
@@ -1,7 +1,14 @@
 package com.cydeo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class InvoiceProductDto {
 
     private Long id;
