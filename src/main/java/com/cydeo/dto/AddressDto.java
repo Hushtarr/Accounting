0a22c9572f6324adabfb4f72e5cc6596acbd4eb0
Adela-cloud <dilov222@gmail.com>
2024-07-24 16:31:29 +0300

    The validation is added to all the fields for the company Dto

--- src/main/java/com/cydeo/dto/AddressDto.java
+++ src/main/java/com/cydeo/dto/AddressDto.java
@@ -4,6 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -11,11 +16,28 @@ import lombok.Setter;
 public class AddressDto {
 
     private Long id;
+
+    @NotBlank (message = "This is a required field")
+    @Size(min = 2, max = 100, message = "Address should have 2-100 characters long")
     private String addressLine1;
+
+    @Size(min=2, max= 100, message = "Address should have maximum 100 characters long")
     private String addressLine2;
+
+    @NotBlank(message = "This is a required field")
+    @Size(min = 2, max = 50, message = "City should have 2-50 characters long")
     private String city;
+
+    @NotBlank(message = "This is a required field")
+    @Size(min = 2, max = 50, message = "State should have 2-50 characters long")
     private String state;
+
+    @NotBlank(message = "This is a required field")
+    @Size(min = 2, max = 50, message = "Country should have 2-50 characters long")
     private String country;
+
+    @NotBlank(message = "This is a required field")
+    @Pattern(regexp = "^\\d{5}-\\d{4}$", message = "Zipcode should be in this format: 'xxxxx-xxxx'")
     private String zipCode;
 
 }
