0b0448a2fd416261668853f5c1d96c2c947a67ca
Asmik <jasmin_ayr@mail.ru>
2024-07-31 00:39:34 +0200

    JDGR4-52-InvoiceDto-clean up

--- src/main/java/com/cydeo/dto/InvoiceDto.java
+++ src/main/java/com/cydeo/dto/InvoiceDto.java
@@ -5,11 +5,9 @@ import com.cydeo.enums.InvoiceType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
